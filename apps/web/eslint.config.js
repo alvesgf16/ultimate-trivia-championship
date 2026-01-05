401390f64160ab9856c13409424f259fc1200a57
Gabriel Alves <alvesgabriel1994@gmail.com>
2026-01-04 22:23:04 -0700

    feat(web): add jsx-a11y and strict rules to linter

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,22 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@repo/config/next-js";
+import { strictConfig } from "@repo/config/strict";
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 /** @type {import("eslint").Linter.Config[]} */
-export default nextJsConfig;
+export default [
+  ...strictConfig,
+  ...nextJsConfig,
+  {
+    plugins: {
+      "jsx-a11y": jsxA11y,
+    },
+    rules: {
+      ...jsxA11y.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "jsx-a11y/anchor-is-valid": "off",
+    },
+  },
+];
