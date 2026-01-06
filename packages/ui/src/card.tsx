7532850733d2658bfd1fda05758b0eb036768405
Gabriel Alves <alvesgabriel1994@gmail.com>
2026-01-05 18:44:02 -0700

    fix(card): fix type definition and href formatting

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -5,16 +5,16 @@ export function Card({
   title,
   children,
   href,
-}: {
+}: Readonly<{
   className?: string;
   title: string;
   children: React.ReactNode;
   href: string;
-}): JSX.Element {
+}>): JSX.Element {
   return (
     <a
       className={className}
-      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo"`}
+      href={`${href}?utm_source=create-turbo&utm_medium=basic&utm_campaign=create-turbo`}
       rel="noopener noreferrer"
       target="_blank"
     >
